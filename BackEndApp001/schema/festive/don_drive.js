fe6727bb55f9139871717e5122cd9b1a0950ec40
Aditya Mishra <aditya.mishra@upscale.vc>
2017-07-20 14:44:13 +0530

    added schema for p2dt

--- BackEndApp001/schema/festive/don_drive.js
+++ BackEndApp001/schema/festive/don_drive.js
@@ -1,12 +1,38 @@
-﻿const Individual = new GraphQLObjectType({
+﻿const Don_Drive = new GraphQLObjectType({
+    id: int,
     title: "",
     donation_category: "", //(Clothes, Books, Stationery, Food, Blood, Water, Sanitary Products, Medicine)
-    beneficiries: {
+    beneficieries: {
         afflicted_by: "",
         located_at: ""
     },
     collection_target: int, //(Range)
-    host: "",
-})
+    collection_till: int, //(Deadline)
+    reward: "",
+    point_multiplier: 2,
+    host: "", //(Automatic)
+    person_incharge: "", //(User Input)
+    contact_info_PIC: "+##-#####-#####",
+    point_of_contact: "",
+    contact_info_POC: "+##-#####-#####",
+    pictures: {
+        drive_start: "",
+        donations_start: "",
+        donations_midway: "",
+        donations_end: "",
 
-module.exports = Individual;
+    },
+    description: {
+        long: "upto 2000 words",
+        short: "upto 60 words, preferably less"
+    },
+    collection_mode: "Home Pickup/ Drop off",
+    progress: {
+        currently_donated: int,
+        currently_pledged: int,
+        actual_donors: int,
+        potential_donors: int,
+    }
+});
+
+module.exports = Don_Drive;
